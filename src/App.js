97695d341e1fc45ec4848f22fa7942248e9b2d82
thejujoe <thejujoe1996@gmail.com>
2023-07-28 00:06:07 +0530

    Added router and used as button click to display each component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Photos from './components/Photos';
-
+import { Link, Route } from "wouter";
+import bootstrap from 'bootstrap';
+import ProfilesComponent from './components/comments';
 
 function App() {
   return (
     <div className="App">
-      <Photos/>
-        
+      <Link href="/Photos.jsx">
+        <button className="btn btn-primary m-2">Photos</button>
+      </Link>
+      <Link href="/comments.jsx">
+        <button className="btn btn-primary me-2">Comments</button>
+      </Link>
+      <Route path="/Photos.jsx" component={Photos} />
+      <Route path="/comments.jsx" component={ProfilesComponent} />
+  
     </div>
   );
 }
